e56e08ee11ebf2a05887f78695b3df0477bc2bbb
coolvbucks <83545358+coolvbucks@users.noreply.github.com>
2021-05-03 01:58:29 +1000

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -34,7 +34,7 @@ $( document ).ready( function() {
 
         $( '.activityContent' ).hide().prepend('<div class="live-stats">' +
             '<div class="avatar"><img src="'+avatar+'" alt="" /></div>' +
-            '<div class="flag"><img src="IMG/flags/'+flag+'.png" alt="" /></div>' +
+            '<div class="flag"><img src="IMG/vBucks.png" alt="" /></div>' +
            '<div class="info">' +
               '<ul>' +
                  '<li><b>'+user+',</b> generated now:</li>' +
